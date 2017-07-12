fc7e2fcf1b1ed04b578018393532c5ee4f030843
Cristian Pereyra <cristian@herolens.com>
2017-07-12 18:43:46 -0300

    remove files

--- dist/metadata/resolveType.d.ts
+++
@@ -1,7 +0,0 @@
-import * as ts from 'typescript';
-import { Type } from './metadataGenerator';
-export declare function resolveType(typeNode?: ts.TypeNode, genericTypeMap?: Map<String, ts.TypeNode>): Type;
-export declare function getSuperClass(node: ts.ClassDeclaration, typeArguments?: Map<String, ts.TypeNode>): {
-    type: any;
-    typeArguments: Map<String, ts.TypeNode>;
-};
--- dist/utils/jsDocUtils.d.ts
+++
@@ -1,4 +0,0 @@
-import * as ts from 'typescript';
-export declare function getJSDocDescription(node: ts.Node): string;
-export declare function getJSDocTag(node: ts.Node, tagName: string): string;
-export declare function isExistJSDocTag(node: ts.Node, tagName: string): boolean;
