88a9385ef0c244fc847accd0aac5c651978684be
thiagobustamante <thiago@cruxframework.org>
2017-10-30 10:04:59 -0200

    Fix default version on generated swagger

--- src/cli.ts
+++ src/cli.ts
@@ -6,10 +6,18 @@ import { Config, SwaggerConfig } from './config';
 import { MetadataGenerator } from './metadata/metadataGenerator';
 import { SpecGenerator } from './swagger/generator';
 
+const packageJson = require(`../package.json`);
+
+const workingDir: string = process.cwd();
+const versionDefault = getPackageJsonValue('version');
+const nameDefault = getPackageJsonValue('name');
+const descriptionDefault = getPackageJsonValue('description');
+const licenseDefault = getPackageJsonValue('license');
+
 const parser = new ArgumentParser({
     addHelp: true,
     description: 'Tree-Gateway Swagger tool',
-    version: '0.0.10'
+    version: packageJson.version
 });
 
 parser.addArgument(
@@ -18,49 +26,6 @@ parser.addArgument(
         help: 'The swagger config file (swagger.json).'
     }
 );
-
-const getPackageJsonValue = (key: string): string => {
-    try {
-        const packageJson = require(`${workingDir}/package.json`);
-        return packageJson[key] || '';
-    } catch (err) {
-        return '';
-    }
-};
-
-const versionDefault = getPackageJsonValue('version');
-const nameDefault = getPackageJsonValue('name');
-const descriptionDefault = getPackageJsonValue('description');
-const licenseDefault = getPackageJsonValue('license');
-
-const getConfig = (configPath = 'swagger.json'): Config => {
-    try {
-        return require(`${workingDir}/${configPath}`);
-    } catch (err) {
-        if (err.code === 'MODULE_NOT_FOUND') {
-            throw Error(`No config file found at '${configPath}'`);
-        } else if (err.name === 'SyntaxError') {
-            throw Error(`Invalid JSON syntax in config at '${configPath}': ${err.message}`);
-        } else {
-            throw Error(`Unhandled error encountered loading '${configPath}': ${err.message}`);
-        }
-    }
-};
-
-const validateSwaggerConfig = (config: SwaggerConfig): SwaggerConfig => {
-    if (!config.outputDirectory) { throw new Error('Missing outputDirectory: onfiguration most contain output directory'); }
-    if (!config.entryFile) { throw new Error('Missing entryFile: Configuration must contain an entry point file.'); }
-    config.version = config.version || versionDefault;
-    config.name = config.name || nameDefault;
-    config.description = config.description || descriptionDefault;
-    config.license = config.license || licenseDefault;
-    config.basePath = config.basePath || '/';
-    config.yaml = config.yaml || true;
-
-    return config;
-};
-
-const workingDir: string = process.cwd();
 
 const parameters = parser.parseArgs();
 const config = getConfig(parameters.config);
@@ -74,3 +39,39 @@ new SpecGenerator(metadata, swaggerConfig).generate(swaggerConfig.outputDirector
     .catch((err: any) => {
         console.error(`Error generating swagger. ${err}`);
     });
+
+function getPackageJsonValue(key: string): string {
+    try {
+        const projectPackageJson = require(`${workingDir}/package.json`);
+        return projectPackageJson[key] || '';
+    } catch (err) {
+        return '';
+    }
+}
+
+function getConfig(configPath = 'swagger.json'): Config {
+    try {
+        return require(`${workingDir}/${configPath}`);
+    } catch (err) {
+        if (err.code === 'MODULE_NOT_FOUND') {
+            throw Error(`No config file found at '${configPath}'`);
+        } else if (err.name === 'SyntaxError') {
+            throw Error(`Invalid JSON syntax in config at '${configPath}': ${err.message}`);
+        } else {
+            throw Error(`Unhandled error encountered loading '${configPath}': ${err.message}`);
+        }
+    }
+}
+
+function validateSwaggerConfig(conf: SwaggerConfig): SwaggerConfig {
+    if (!conf.outputDirectory) { throw new Error('Missing outputDirectory: onfiguration most contain output directory'); }
+    if (!conf.entryFile) { throw new Error('Missing entryFile: Configuration must contain an entry point file.'); }
+    conf.version = conf.version || versionDefault;
+    conf.name = conf.name || nameDefault;
+    conf.description = conf.description || descriptionDefault;
+    conf.license = conf.license || licenseDefault;
+    conf.basePath = conf.basePath || '/';
+    conf.yaml = conf.yaml || true;
+
+    return conf;
+}
