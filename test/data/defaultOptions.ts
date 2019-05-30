1ebe71d8b4cd8192d907e8632480f91e794340e7
Jack Hicks <jack@thrive.uk.com>
2019-05-30 11:07:50 +0100

    altered tests to match new argument structure (and included securityDefinitions in the actual defaults for proper testing)

--- test/data/defaultOptions.ts
+++ test/data/defaultOptions.ts
@@ -1,15 +1,41 @@
-import { SwaggerConfig } from './../../src/config';
+import {SwaggerConfig} from './../../src/config';
+
 export function getDefaultOptions(): SwaggerConfig {
-  return {
-    basePath: '/',
-    collectionFormat: 'multi',
-    description: 'Description of a test API',
-    entryFile: '',
-    host: 'localhost:3000',
-    license: 'MIT',
-    name: 'Test API',
-    outputDirectory: '',
-    version: '1.0.0',
-    yaml: false
-  };
+    return {
+        basePath: '/',
+        collectionFormat: 'multi',
+        description: 'Description of a test API',
+        entryFile: '',
+        host: 'localhost:3000',
+        license: 'MIT',
+        name: 'Test API',
+        outputDirectory: '',
+        'securityDefinitions': {
+            'access_token': {
+                'in': 'header',
+                'name': 'authorization',
+                'type': 'apiKey'
+            },
+            'api_key': {
+                'in': 'query',
+                'name': 'access_token',
+                'type': 'apiKey',
+            },
+            'user_email': {
+                'in': 'header',
+                'name': 'x-user-email',
+                'type': 'apiKey'
+            }
+        },
+        'spec': {
+            'api_key': {
+                'in': 'header',
+                'name': 'api_key',
+                'type': 'apiKey'
+            }
+        },
+        version: '1.0.0',
+        yaml: false,
+
+    };
 }
