5ca3ebf783ac2c00e3d3757dcd2c42fa93876c56
juliensergent <julien.sergent@kubity.com>
2019-07-17 22:40:46 +0200

    add test and avoid to exclude type of typescript-rest-swagger

--- src/metadata/metadataGenerator.ts
+++ src/metadata/metadataGenerator.ts
@@ -28,7 +28,7 @@ export class MetadataGenerator {
         this.program.getSourceFiles().forEach(sf => {
             if (this.ignorePaths && this.ignorePaths.length) {
                 for (const path of this.ignorePaths) {
-                    if(mm(sf.fileName, path)) {
+                    if(!sf.fileName.includes('node_modules/typescript-rest-swagger/') && mm(sf.fileName, path)) {
                         return;
                     }
                 }
